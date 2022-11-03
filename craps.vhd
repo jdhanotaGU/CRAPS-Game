dcd69df4fde9b37447a07da5d1a564f914f03d0f
gbockenstedt <gbockenstedt@zagmail.gonzaga.edu>
2022-11-03 14:11:39 -0700

    Next state logic and D-Flip logic are done. Not sure if they will work as intended. Need to combine our code.

--- craps.vhd
+++ craps.vhd
@@ -19,12 +19,15 @@ signal firstRoll: std_logic := '0'; -- 0 if this is the first roll. If on 2nd+ r
 signal nextGame: std_logic := '0'; -- Signal that the next game is starting (update previousSum)
 signal currentSum: integer range 0 to 12;
 signal previousSum: integer range 0 to 12;
+signal dice1Cnt: integer range 0 to 7;
+signal dice2Cnt: integer range 0 to 7;
 	
 begin
 	craps_nextStateLogic_p: process(currentState)
 	begin
 		case currentState is
 			when waitPress => 
+				nextGame <= '0'; -- reset
 				if (key_clean = '1') then
 					nextState <= pressRecieved;
 				else
@@ -57,12 +60,33 @@ begin
 			
 			when lose => -- loop forever here
 				nextState <= lose;
-		
 		end case;
 	end process craps_nextStateLogic_p;
 	
 	craps_DFlipFlop: process(MCLK, rst)
 	begin
+		if (rst = '0') then
+			firstRoll <= '0';
+			dice1Cnt <=  0;
+			dice2Cnt <= 0;
+			currentSum <= 0;
+			previousSum <= 0;
+			currentState <= waitPress;
+		elsif (rising_edge(MCLK)) then
+			currentState <= nextState;
+			if (key_clean = '1') then
+				if (firstRoll = '0') then -- This is the first game
+					firstRoll <= '1';
+				else
+					firstRoll <= '0'; -- prevent latch
+				end if;
+				currentSum <= dice1Cnt + dice2Cnt;
+			end if;
+			
+			if (nextGame = '1') then
+				previousSum <= currentSum; -- Is this ok? Read and write ok on currentSum?
+			end if;
+		end if;
 	
 	end process craps_DFlipFlop;
 	
