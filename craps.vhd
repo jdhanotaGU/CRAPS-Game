730b48857b7e4c307c10bc9b66f1f6d308688a40
gbockenstedt <gbockenstedt@zagmail.gonzaga.edu>
2022-11-03 13:04:31 -0700

    Updated with layout

--- craps.vhd
+++ craps.vhd
@@ -3,12 +3,40 @@ USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
 
 entity craps is
-port (rst: in std_logic;
-		clk_in: in std_logic; -- Clk_in is 50MHZ
+port (rst, clk_in, key: in std_logic;
 		slow_clk: out std_logic); -- slow_clk is the slowed down clock
 end craps;
 
 architecture rtl of craps is
 begin
+	
+	signal key_clean: std_logic := '0'; -- Holds the clean key press. (Cleaned using negative edge detection)
+	
+	craps_nextStateLogic_p: process(currentState, key)
+	begin
+	
+	end process craps_nextStateLogic_p;
+	
+	craps_DFlipFlop: process(slow_clk_in, reset)
+	begin
+	
+	end process craps_DFlipFlop;
+	
+	craps_outputLogic_p: process(currentState, nextState)
+	begin
+	
+	end process craps_outputLogic_p;
+	
+	
+	craps_negEdgeDetect: process(slow_clk_in, key) -- detects negative edges of the key (debouncer)
+	 variable sigA: std_logic;
+	 variable sigB: std_logic;
+	 begin
+		sigA := key;
+		if (rising_edge(slow_clk_in)) then
+			sigB := key; 
+		end if;
+		key_clean <= not sigA and sigB;
+	end process craps_negEdgeDetect;
 	 
 end rtl;
