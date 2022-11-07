f6c58fbea7f15a32c30aed5f9ffdd60308e3be8f
gbockenstedt <gbockenstedt@zagmail.gonzaga.edu>
2022-11-07 12:43:52 -0800

    More stuff

---
+++ craps.tcl
@@ -0,0 +1,42 @@
+set_location_assignment PIN_AA17 -to seg1[0]
+set_location_assignment PIN_AB16 -to seg1[1]
+set_location_assignment PIN_AA16 -to seg1[2]
+set_location_assignment PIN_AB17 -to seg1[3]
+set_location_assignment PIN_AB15 -to seg1[4]
+set_location_assignment PIN_AA15 -to seg1[5]
+set_location_assignment PIN_AC17 -to seg1[6]
+
+set_location_assignment PIN_AB19 -to seg2[0]
+set_location_assignment PIN_AA19 -to seg2[1]
+set_location_assignment PIN_AG21 -to seg2[2]
+set_location_assignment PIN_AH21 -to seg2[3]
+set_location_assignment PIN_AE19 -to seg2[4]
+set_location_assignment PIN_AF19 -to seg2[5]
+set_location_assignment PIN_AE18 -to seg2[6]
+
+set_location_assignment PIN_M24 -to seg3[0]
+set_location_assignment PIN_Y22 -to seg3[1]
+set_location_assignment PIN_W21 -to seg3[2]
+set_location_assignment PIN_W22 -to seg3[3]
+set_location_assignment PIN_W25 -to seg3[4]
+set_location_assignment PIN_U23 -to seg3[5]
+set_location_assignment PIN_U24 -to seg3[6]
+
+set_location_assignment PIN_G18 -to seg4[0]
+set_location_assignment PIN_F22 -to seg4[1]
+set_location_assignment PIN_E17 -to seg4[2]
+set_location_assignment PIN_L26 -to seg4[3]
+set_location_assignment PIN_L25 -to seg4[4]
+set_location_assignment PIN_J22 -to seg4[5]
+set_location_assignment PIN_H22 -to seg4[6]
+
+set_location_assignment PIN_E21 -to win_LED
+set_location_assignment PIN_G21 -to lose_LED
+
+set_location_assignment PIN_R24 -to key
+
+set_location_assignment PIN_Y2 -to MCLK
+
+set_location_assignment PIN_M23 -to rst
+
+set_location_assignment PIN_G19 -to debugLED
--- craps.vhd
+++ craps.vhd
@@ -3,8 +3,10 @@ USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
 
 entity craps is
-port (rst, MCLK, key: in std_logic;
-		slow_clk: out std_logic); -- slow_clk is the slowed down clock
+port (reset, MCLK, key: in std_logic;
+		seg1, seg2, seg3, seg4: out std_logic_vector(6 downto 0);	-- outputs for 7 seg displays
+		debugLED, win_LED, lose_LED: out std_logic	-- LEDs for win and lose
+		);
 end craps;
 
 architecture rtl of craps is
@@ -14,20 +16,37 @@ type stateType is (waitPress, pressRecieved, win, lose);
 -- Signals:
 signal nextState: stateType := waitPress;
 signal currentState: stateType := waitPress;
+signal slow_clk_in: std_logic; -- This will act as our slow clock. Updated by the counter component.
 signal key_clean: std_logic := '0'; -- Holds the clean key press. (Cleaned using negative edge detection)
 signal firstRoll: std_logic := '0'; -- 0 if this is the first roll. If on 2nd+ roll, then 1.
 signal nextGame: std_logic := '0'; -- Signal that the next game is starting (update previousSum)
+signal stopCount: std_logic := '0';
 signal currentSum: integer range 0 to 12;
 signal previousSum: integer range 0 to 12;
 signal dice1Cnt: integer range 0 to 7;
 signal dice2Cnt: integer range 0 to 7;
+signal seg1_output : std_logic_vector(6 downto 0);
+signal seg2_output : std_logic_vector(6 downto 0);
+signal seg3_output : std_logic_vector(6 downto 0);
+signal seg4_output : std_logic_vector(6 downto 0);
+
+-- Instatiate the counter component:
+component counter
+	port (rst: in std_logic;
+		clk_in: in std_logic;
+		slow_clk: out std_logic); -- clock_out is the slowed down clock (4ms period)
+end component counter;
 	
 begin
+	-- Component Definitions --
+	slowClock : counter port map(rst => reset, clk_in => MCLK, slow_clk => slow_clk_in);
+	
 	craps_nextStateLogic_p: process(currentState)
 	begin
 		case currentState is
 			when waitPress => 
 				nextGame <= '0'; -- reset
+				stopCount <= '0';
 				if (key_clean = '1') then
 					nextState <= pressRecieved;
 				else
@@ -38,42 +57,69 @@ begin
 				if (firstRoll = '1') then
 					if (currentSum = 7 or currentSum = 11) then
 						nextState <= win;
+						stopCount <= '1';
 					elsif (currentSum = 2 or currentSum = 3 or currentSum = 12) then
 						nextState <= lose;
+						stopCount <= '1';
 					else -- nextGame
+						stopCount <= '0';
 						nextGame <= '1';
 						nextState <= waitPress;
 					end if;
 				else -- if this is the 2nd+ roll
 					if (currentSum = previousSum) then
 						nextState <= win;
+						stopCount <= '1';
 					elsif (currentSum = 7) then
 						nextState <= lose;
+						stopCount <= '1';
 					else -- Next game
 						nextGame <= '1';
 						nextState <= waitPress;
+						stopCount <= '0';
 					end if;
 				end if;
 			
 			when win => -- Loop forever here
+				stopCount <= '1';
 				nextState <= win;
 			
 			when lose => -- loop forever here
+				stopCount <= '1';
 				nextState <= lose;
 		end case;
 	end process craps_nextStateLogic_p;
 	
-	craps_DFlipFlop: process(MCLK, rst)
+	craps_DFlipFlop: process(slow_clk_in, reset, stopCount)
 	begin
-		if (rst = '0') then
+		if (reset = '0') then
 			firstRoll <= '0';
 			dice1Cnt <=  0;
 			dice2Cnt <= 0;
 			currentSum <= 0;
 			previousSum <= 0;
 			currentState <= waitPress;
-		elsif (rising_edge(MCLK)) then
+		elsif (rising_edge(slow_clk_in)) then
 			currentState <= nextState;
+			if (stopCount = '0') then
+				if (dice1Cnt >= 6) then
+					dice1Cnt <= 0;
+				else
+					dice1Cnt <= dice1Cnt + 1;
+				end if;
+				if ((dice1Cnt mod 3) = 0) then
+					if (dice2Cnt >= 6) then
+						dice2Cnt <= 0;
+					else
+						dice2Cnt <= dice2Cnt + 1;
+					end if;
+				else 
+					dice2Cnt <= dice2Cnt;
+				end if;
+			else
+				dice1Cnt <= dice1Cnt;
+				dice2Cnt <= dice2Cnt;
+			end if;
 			if (key_clean = '1') then
 				if (firstRoll = '0') then -- This is the first game
 					firstRoll <= '1';
@@ -90,18 +136,123 @@ begin
 	
 	end process craps_DFlipFlop;
 	
+	craps_segmentLogic_p: process(dice1Cnt, dice2Cnt, currentSum)
+	begin
+		case dice1Cnt is
+			when 1 =>
+				seg1_output <= "0000110";
+			when 2 =>
+				seg1_output <= "1011011";
+			when 3 =>
+				seg1_output <= "1001111";
+			when 4 => 
+				seg1_output <= "1100110";
+			when 5 =>
+				seg1_output <= "1101101";
+			when 6 =>
+				seg1_output <= "1111101";
+			when others =>
+				seg1_output <= "0000000";
+		end case;
+		case dice2Cnt is
+			when 1 =>
+				seg2_output <= "0000110";
+			when 2 =>
+				seg2_output <= "1011011";
+			when 3 =>
+				seg2_output <= "1001111";
+			when 4 => 
+				seg2_output <= "1100110";
+			when 5 =>
+				seg2_output <= "1101101";
+			when 6 =>
+				seg2_output <= "1111101";
+			when others =>
+				seg2_output <= "0000000";
+		end case;
+		case currentSum is
+			when 2 =>
+				seg3_output <= "0000000";
+				seg4_output <= "1011011";
+			when 3 =>
+				seg3_output <= "0000000";
+				seg4_output <= "1001111";
+			when 4 => 
+				seg3_output <= "0000000";
+				seg4_output <= "1100110";
+			when 5 =>
+				seg3_output <= "0000000";
+				seg4_output <= "1101101";
+			when 6 =>
+				seg3_output <= "0000000";
+				seg4_output <= "1111101";
+			when 7 =>
+				seg3_output <= "0000000";
+				seg4_output <= "0000111";
+			when 8 =>
+				seg3_output <= "0000000";
+				seg4_output <= "1111111";
+			when 9 =>
+				seg3_output <= "0000000";
+				seg4_output <= "1101111";
+			when 10 =>
+				seg3_output <= "0000110";
+				seg4_output <= "0111111";
+			when 11 =>
+				seg3_output <= "0000110";
+				seg4_output <= "0000110";
+			when 12 =>
+				seg3_output <= "0000110";
+				seg4_output <= "1011011";
+			when others =>
+				seg3_output <= "0000000";
+				seg4_output <= "0000000";
+				-- do nothing
+		end case;
+	end process craps_segmentLogic_p;
+	
+	-- I don't believe next state is needed in the sensitivity list
+	-- segments might cause latches since not defined for every case
 	craps_outputLogic_p: process(currentState, nextState)
 	begin
-	
+		case currentState is 
+			when waitPress =>
+				-- display counter 7-seg values constanty updating
+				debugLED <= '0';
+				seg1 <= not seg1_output;
+				seg2 <= not seg2_output;
+				seg3 <= not "0000000";
+				seg4 <= not "0000000";
+				win_LED <= '0';
+				lose_LED <= '0';
+			when pressRecieved =>
+				-- display 7-seg with final value of button pressed
+				-- should display result of first dice role, second dice role, and summed dice role
+				debugLED <= '1';
+				seg1 <= not seg1_output;
+				seg2 <= not seg2_output;
+				seg3 <= not seg3_output;
+				seg4 <= not seg4_output;
+				win_LED <= '0';
+				lose_LED <= '0';
+			when win =>
+				win_LED <= '1';
+				lose_LED <= '0';
+			when lose =>
+				win_LED <= '0';
+				lose_LED <= '1';
+			when others =>
+				-- do nothing
+		end case;
 	end process craps_outputLogic_p;
 	
 	
-	craps_negEdgeDetect: process(MCLK, key) -- detects negative edges of the key (debouncer)
+	craps_negEdgeDetect: process(slow_clk_in, key) -- detects negative edges of the key (debouncer)
 	 variable sigA: std_logic;
 	 variable sigB: std_logic;
 	 begin
 		sigA := key;
-		if (rising_edge(MCLK)) then
+		if (rising_edge(slow_clk_in)) then
 			sigB := key; 
 		end if;
 		key_clean <= not sigA and sigB;
